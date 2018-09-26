d33fd3b43165e4f680971e518fbefeed6c492136
ethanczc <41766483+ethanczc@users.noreply.github.com>
2018-09-26 16:26:21 +0800

    add print methods

---
+++ print_methods.py
@@ -0,0 +1,8 @@
+x=1.5
+y=2
+
+print ("method 1, x: %8.3f y: %i" % (x,y)) # 8 = number of spaces the entire number takes, 3 = number of decimal places, f = float
+print ('method 2, x: ' + str(x) + ' y: ' + str(y))
+print ('method 3, x: {} y: {}'.format(x,y))
+
+print ("%s is %i years old" % ("John", 23)) # %s = string, %i = integer
